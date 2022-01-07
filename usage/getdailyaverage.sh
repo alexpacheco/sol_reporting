b2878cd735dea7fd532940a05e036da6177c2034
Alex Pacheco <alex.pacheco@lehigh.edu>
2022-01-07 07:18:26 -0500

    Added option to follow symlinks for rclone command

--- usage/getdailyaverage.sh
+++ usage/getdailyaverage.sh
@@ -32,6 +32,6 @@ ${GIT} commit -am "Updated Sol Metrics at ${COMMIT_TIMESTAMP}"
 ${GIT} push web master
 
 export PATH=/share/Apps/lusoft/opt/spack/linux-centos8-x86_64/gcc-8.3.1/rclone/1.53.3-meqi3gu/bin:$PATH
-rclone copy -P ~/usage dropbox:sol_reporting/usage
+rclone copy -P -L ~/usage dropbox:sol_reporting/usage
 
 
